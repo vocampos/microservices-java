d739a12b97fa966e1c8fb01cb8048772c2419832
Valbert Campos <valbert.sistemas@gmail.com>
2021-08-27 16:00:54 -0300

    Project hr-eureka-server created

--- hr-payroll/src/main/java/com/example/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/example/hrpayroll/HrPayrollApplication.java
@@ -2,10 +2,10 @@ package com.example.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClients;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClients
+@RibbonClient(name="hr-worker")
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {

b9686eae47b012e41982f82426ef63f1b05ec33b
Valbert Campos <valbert.sistemas@gmail.com>
2021-08-27 20:43:44 -0300

    Project hr-config-server created

--- hr-worker/src/main/java/com/example/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/example/hrworker/resources/WorkerResource.java
@@ -3,6 +3,7 @@ package com.example.hrworker.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,22 +12,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.hrworker.entities.Worker;
 import com.example.hrworker.repositories.WorkerRepository;
+import com.sun.istack.logging.Logger;
 
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
+	
+	private static Logger logger = Logger.getLogger(WorkerResource.class);
+	private static Long qtd = 1L;
+	
+	@Value("${eureka.instance.instance-id}")
+	public String server;
 
 	@Autowired
 	private WorkerRepository repository;
 
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
+		logger.info(server + " QTD" + qtd++);
 		List<Worker> list = repository.findAll();
 		return ResponseEntity.ok(list);
 	}
 
 	@GetMapping(value="/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+		logger.info(server + " QTD" + qtd++);
 		Worker worker = repository.findById(id).get();
 		return ResponseEntity.ok(worker);
 	}

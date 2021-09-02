4f853b336db0a8906e03452e21716804b5d8ac47
Valbert Campos <valbert.sistemas@gmail.com>
2021-09-02 15:36:22 -0300

    hr-user Postgres dev profile

--- hr-config-server/src/main/java/com/example/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/example/hrconfigserver/HrConfigServerApplication.java
@@ -1,6 +1,5 @@
 package com.example.hrconfigserver;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,19 +9,12 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class HrConfigServerApplication implements CommandLineRunner {
 
-	@Value("${spring.cloud.config.server.git.username}")
-	private String username;
-	
-	@Value("${spring.cloud.config.server.git.password}")
-	private String pass;
-
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
 	}
 
 	public void run(String... args) throws Exception {
-		System.out.println("USERNAME: " + username);
-		System.out.println("pass: " + pass);
+		System.out.println("HrConfigServerApplication run");
 	}
 
 }
